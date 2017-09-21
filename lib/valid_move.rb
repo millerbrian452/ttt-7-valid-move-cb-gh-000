bd327cb4dd8323cf37351ba821ca2f1c5eab6489
Brian Miller <jwino0004@mail.ct.edu>
2017-09-21 13:25:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board,position)
-  position.to_i.between?(1,9) and not position_taken?(board, position.to_i - 1)
+  position.between?(1,9) and not position_taken?(board,position)
 end
 
 def position_taken?(board,position)
